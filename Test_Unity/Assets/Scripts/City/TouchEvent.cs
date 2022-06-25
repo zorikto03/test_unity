07545cd9e02c16208e0ce4ede360b78a0cdb68c4
zorikto03 <zorikb17@gmail.com>
2022-06-26 00:49:44 +0800

    added moving controller by touching

--- Test_Unity/Assets/Scripts/City/TouchEvent.cs
+++ Test_Unity/Assets/Scripts/City/TouchEvent.cs
@@ -4,18 +4,107 @@ using UnityEngine;
 
 public class TouchEvent : MonoBehaviour
 {
-    GameObject particle;
+    [Header("ћаксимальна€ скорость сдвига в сторону")]
+    public float StrafSpeed = 5f;
+
+    CharacterController controller;
+    Vector2 startPos = new();
+    Resolution resolution;
+    bool isSwipe;
+    bool isTouch;
+
+    private void Start()
+    {
+        resolution = Screen.currentResolution;
+        controller = GetComponent<CharacterController>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        foreach(Touch touch in Input.touches)
+        if (Input.touchCount > 0)
         {
-            Ray ray = Camera.main.ScreenPointToRay(touch.position);
-            if (Physics.Raycast(ray))
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
             {
-                Instantiate(particle, transform.position, transform.rotation);
+                case TouchPhase.Began:
+                    startPos = touch.position;
+                    StartCoroutine(Jump());
+                    break;
+                case TouchPhase.Stationary:
+                case TouchPhase.Moved:
+                    isSwipe = true;
+                    var swipeDistHorizontal = (touch.position.x - startPos.x) * -1;
+                    var procent = swipeDistHorizontal / resolution.width;
+
+                    Vector3? direction = null;
+                    if (swipeDistHorizontal < 0)//Right
+                    {
+                        direction = new Vector3(0, 0, procent);
+                        Debug.Log($"Right: {swipeDistHorizontal}; width: {resolution.width}: proc: {procent}");
+                    }
+                    else if(swipeDistHorizontal > 0)//Left
+                    {
+                        direction = new Vector3(0, 0, procent);
+                        Debug.Log($"Left: {swipeDistHorizontal}; width: {resolution.width}: proc: {procent}");
+                    }
+
+                    if (direction is not null)
+                    {
+                        //if (controller.transform.position.z < 4.5f || controller.transform.position.z > -4.5f)
+                        //{
+                        //}
+                        controller.Move((Vector3)direction * StrafSpeed * Time.deltaTime);
+                    }
+
+                    break;
+                case TouchPhase.Ended:
+                    isSwipe = false;
+                    isTouch = false;
+                    startPos = new();
+                break;
             }
         }
+        //foreach(Touch touch in Input.touches)
+        //{
+        //    Ray ray = Camera.main.ScreenPointToRay(touch.position);
+        //    if (Physics.Raycast(ray))
+        //    {
+        //        Instantiate(particle, transform.position, transform.rotation);
+        //    }
+        //}
+    }
+
+    IEnumerator Jump()
+    {
+        if (!isSwipe)
+        {
+            yield return new WaitForSeconds(0.05f);
+            if (!isSwipe)
+            {
+                Debug.Log("Tap");
+
+            }
+            else
+            {
+                yield break;
+            }
+        }
+        else
+        {
+            yield break;
+        }
+    }
+
+    IEnumerator Left()
+    {
+        Debug.Log("Left");
+        yield return new WaitForSeconds(0.05f);
+    }
+    IEnumerator Right()
+    {
+        Debug.Log("Right");
+        yield return new WaitForSeconds(0.05f);
     }
 }
